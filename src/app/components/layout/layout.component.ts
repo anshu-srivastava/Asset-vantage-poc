e7f3b2b375bf8c2184a1a947b2998d85595dbb79
anshu-coditas <anshu.srivastava@coditas.com>
2020-04-15 11:07:34 +0530

    changes for layout component

---
+++ src/app/components/layout/layout.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { GridsterConfig, GridsterItem } from 'angular-gridster2';
+import { LayoutService } from 'src/app/services/layout.service';
+
+@Component({
+  selector: 'app-layout',
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.scss']
+})
+export class LayoutComponent implements OnInit {
+
+  constructor(private layoutService: LayoutService) { }
+
+  ngOnInit() {
+  }
+
+  get options(): GridsterConfig {
+    return this.layoutService.options;
+  }
+
+  get layout(): GridsterItem[] {
+    return this.layoutService.layout;
+  }
+
+}
