40efab94cf5a820b50f809062f6c7697e1f0d32d
anshu-coditas <anshu.srivastava@coditas.com>
2020-04-20 11:13:22 +0530

    changes for grid layout

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { GridsterConfig, GridsterItem } from 'angular-gridster2';
+import { Component, OnInit, ChangeDetectionStrategy, ViewEncapsulation } from '@angular/core';
+import { GridsterConfig, GridsterItem, GridsterItemComponent, GridType, CompactType, GridsterPush } from 'angular-gridster2';
 import { LayoutService } from 'src/app/services/layout.service';
 import * as jspdf from 'jspdf';
 import html2canvas from 'html2canvas';
@@ -7,20 +7,63 @@ import html2canvas from 'html2canvas';
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss']
+  styleUrls: ['./layout.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None
 })
 export class LayoutComponent implements OnInit {
+  options: GridsterConfig;
+  dashboard: Array<GridsterItem>;
+  itemToPush: GridsterItemComponent;
+
   constructor(private layoutService: LayoutService) { }
 
   ngOnInit() {
+    this.options = {
+      gridType: GridType.Fit,
+      compactType: CompactType.None,
+      pushItems: true,
+      draggable: {
+        enabled: true
+      },
+      resizable: {
+        enabled: true
+      }
+    };
+
+    this.dashboard = [
+      {cols: 2, rows: 1, y: 0, x: 0, initCallback: this.initItem.bind(this)},
+      {cols: 2, rows: 2, y: 0, x: 2},
+      {cols: 1, rows: 1, y: 0, x: 4},
+      {cols: 3, rows: 2, y: 1, x: 4},
+      {cols: 1, rows: 1, y: 4, x: 5},
+      {cols: 1, rows: 1, y: 2, x: 1},
+      {cols: 2, rows: 2, y: 5, x: 5},
+      {cols: 2, rows: 2, y: 3, x: 2},
+      {cols: 2, rows: 1, y: 2, x: 2},
+      {cols: 1, rows: 1, y: 3, x: 4},
+      {cols: 1, rows: 1, y: 0, x: 6}
+    ];
   }
 
-  get options(): GridsterConfig {
-    return this.layoutService.options;
+  changedOptions() {
+    if (this.options.api && this.options.api.optionsChanged) {
+      this.options.api.optionsChanged();
+    }
   }
 
-  get layout(): GridsterItem[] {
-    return this.layoutService.layout;
+  removeItem($event, item) {
+    $event.preventDefault();
+    $event.stopPropagation();
+    this.dashboard.splice(this.dashboard.indexOf(item), 1);
+  }
+
+  addItem() {
+    this.dashboard.push({x: 0, y: 0, cols: 1, rows: 1});
+  }
+
+  initItem(item: GridsterItem, itemComponent: GridsterItemComponent) {
+    this.itemToPush = itemComponent;
   }
 
   exportDashboard() {
